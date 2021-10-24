28a53609648b52c04d064a58640be0ef08d0d961
RiceDaddy321 <69203443+RiceDaddy321@users.noreply.github.com>
2021-10-24 12:15:48 -0500

    Mergesort implementation (#1)
    * Finished loadFromFile(string file) in the <string> template class for splitmerge. Also managed to template the class.
    
    * Modified and successfully implemented loadFromFile.h. Removed unnecessary comments.
    
    * Includes example of output file. writetofile works and has good readability.
    
    * Sortedwhale output, and added timing mechanism for the sort method.
    
    * Sortedwhale output, and added timing mechanism for the sort method.
    
    * modified function to use mergesort, but it breaks, so next modification will be to test it on the smaller list.
    
    * Finally got the sorting algo to work
    
    * successful implementation of mergesort. Completes in 7 seconds.
    
    * Final code.

--- linkedlist.h
+++ linkedlist.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-#include <sstream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -13,7 +13,7 @@ private:
 	public:
 		node* next;
 		node* prev;
-		int data;
+		T data;
 
 		node()
 			:next(nullptr), prev(nullptr), data(0)
@@ -21,250 +21,13 @@ private:
 
 		}
 
-		node(int _data)
-			:data(_data)
-		{
-
-		}
-
-		node(int _data, node* _next, node* _prev)
-			:data(_data), next(_next), prev(_prev)
-		{
-
-		}
-	};
-
-	node* head;
-	node* tail;
-
-	bool isListEmpty()
-	{
-		if (head == nullptr && tail == nullptr)
-			return true;
-		else
-			return false;
-	}
-
-	node* findSmallest(node* p)
-	{
-		node* smallest = p;
-		for (node* iptr = p; iptr != nullptr; iptr = iptr->next)
-		{
-			if (iptr->data < smallest->data)
-				smallest = iptr;
-		}
-
-		return smallest;
-	}
-public:
-	//sets head and tail to nullptr
-	linkedList()
-		:head(nullptr), tail(nullptr)
-	{
-
-	}
-
-	//adds the data to the back of the list
-	void push_back(int _data)
-	{
-		//Create a new node
-		node* baby = new node(_data);
-
-		if (tail == nullptr) //list is empty)
-		{
-			tail = baby;
-			head = baby;
-			baby->next = nullptr;
-			baby->prev = nullptr;
-		}
-		else
-		{
-			//Point old last node's next ptr to baby node
-			tail->next = baby;
-
-			//Point baby's prev ptr to old last node
-			baby->prev = tail;
-
-			//Update tail ptr to be the new baby node
-			tail = baby;
-
-			//set next to nullpre
-			tail->next = nullptr;
-		}
-	}
-
-	void push_back(node* p)
-	{
-		if (tail == nullptr)
-		{
-			tail = p;
-			head = p;
-			p->next = nullptr;
-			p->prev = nullptr;
-		}
-		else
-		{
-			tail->next = p;
-			p->prev = tail;
-			tail = p;
-			tail->next = nullptr;
-		}
-	}
-
-	void push_front(node* p)
-	{
-		if (head == nullptr)
-		{
-			head = p;
-			tail = p;
-			p->next = nullptr;
-			p->prev = nullptr;
-		}
-		else
-		{
-			head->prev = p;
-			p->next = head;
-			head = p;
-			head->prev = nullptr;
-		}
-	}
-
-	//A simple print method
-	void print()
-	{
-		//check if empty
-		if (isListEmpty)
-		{
-			cout << "(empty)" << endl;
-			return;
-		}
-		else
-		{
-			for (node* iptr = head; iptr != nullptr; iptr = iptr->next)
-			{
-				cout << iptr->data << endl << endl;
-			}
-		}
-	}
-
-	//splits the contents of the current object in two halves. Each half goes to the left and right respectively. Runs in O(n)
-	void split(linkedList& left, linkedList& right)
-	{
-		while (tail->next != head)
-		{
-			//when the list is odd
-			if (head == tail)
-			{
-				left.push_back(head);
-			}
-			else
-			{
-				right.push_back(tail);
-				left.push_back(head);
-			}
-
-			head = head->next;
-			tail = tail->prev;
-		}
-		
-		head = nullptr;
-		tail = nullptr;
-	}
-
-	//Uses simple selection sort
-	void sort()
-	{
-		for (node* iptr = head; iptr != nullptr; iptr = iptr->next)
-		{
-			//findsmallest
-			node* small = findSmallest(iptr);
-
-			//swap smallest to node iptr
-			swap(iptr->data, small->data);
-		}
-	}
-
-	//Implement a method that takes 2 sorted lists and merges them
-	//into your (initially empty) list to create one sorted list.
-	//Must run in O(n) time, where n is the total number of items in the two given lists.
-	void merge(linkedList& left, linkedList& right)
-	{
-		//we want to loop until the shortest ends, then attach the remaining list to this
-		while (left.head != nullptr && right.head != nullptr)
-		{
-			if (left.head->data > right.head->data)
-			{
-				push_back(right.head->data);
-				right.head = right.head->next;
-			}
-			else if (left.head->data < right.head->data)
-			{
-				push_back(left.head->data);
-				left.head = left.head->next;
-			}
-			else
-			{
-				push_back(left.head->data);
-				left.head = left.head->next;
-			}
-		}
-
-		//if the left is empty
-		if (left.head != nullptr)
-		{
-			//get the remaining items from the right
-			tail->next = right.head;
-			right.head->prev = tail;
-		}
-		else if (right.head != nullptr)
-		{
-			//get the remaining items from the left
-			tail->next = left.head;
-			left.head->prev = tail;
-		}
-
-		//M.I.B. the left and right
-		left.head = nullptr;
-		left.tail = nullptr;
-		
-		right.head = nullptr;
-		right.tail = nullptr;
-	}
-
-	//loadFromFile should open the file of the specified name and
-	//enter each item (words in the novel "Moby Dick" in this case)
-	//in the linked list.
-	void loadFromFile(string file);
-	
-	//writeToFile should write each item in the linked list to the provided output file.
-	//In this example, it should write all the words from Moby Dick to the provided output file in sorted order.
-	void writeToFile(string file);
-};
-
-template<>
-class linkedList <double>
-{
-private:
-	class node
-	{
-	public:
-		node* next;
-		node* prev;
-		double data;
-
-		node()
-			:next(nullptr), prev(nullptr), data(0)
-		{
-
-		}
-
-		node(double _data)
+		node(T _data)
 			:data(_data), next(nullptr), prev(nullptr)
 		{
 
 		}
 
-		node(double _data, node* _next, node* _prev)
+		node(T _data, node* _next, node* _prev)
 			:data(_data), next(_next), prev(_prev)
 		{
 
@@ -282,16 +45,29 @@ private:
 			return false;
 	}
 
-	node* findSmallest(node* p)
+	void mergeSort(linkedList& list)//doesn't need an array parameter because it has this
 	{
-		node* smallest = p;
-		for (node* iptr = p; iptr != nullptr; iptr = iptr->next)
+		if (list.head != nullptr
+			&& list.tail != nullptr
+			&& list.tail != list.head)//when we have more than one item 
 		{
-			if (iptr->data < smallest->data)
-				smallest = iptr;
+			//split the array into smaller pieces
+			linkedList<T> left;
+			linkedList<T> right;
+
+			list.split(left, right);
+
+			//do the left
+			list.mergeSort(left);
+			//do the right now
+			list.mergeSort(right);
+
+			//start piecing them back together
+			//call merge function
+			list.merge(left, right);
 		}
 
-		return smallest;
+		return;//if a single item then return
 	}
 public:
 	//sets head and tail to nullptr
@@ -301,8 +77,7 @@ public:
 
 	}
 
-	//adds the data to the back of the list
-	void push_back(double _data)
+	void push_back(T _data)
 	{
 		//Create a new node
 		node* baby = new node(_data);
@@ -336,7 +111,7 @@ public:
 		{
 			tail = p;
 			head = p;
-			
+
 			tail->prev = nullptr;
 		}
 		else
@@ -344,7 +119,7 @@ public:
 			tail->next = p;
 			p->prev = tail;
 			tail = p;
-			
+
 		}
 	}
 
@@ -355,14 +130,14 @@ public:
 			head = p;
 			tail = p;
 			head->next = nullptr;
-			
+
 		}
 		else
 		{
 			head->prev = p;
 			p->next = head;
 			head = p;
-			
+
 		}
 	}
 
@@ -388,47 +163,67 @@ public:
 	//splits the contents of the current object in two halves. Each half goes to the left and right respectively. Runs in O(n)
 	void split(linkedList& left, linkedList& right)
 	{
-		while (tail->next != head && head != nullptr && tail != nullptr)
+		//check if nullptr
+		if (tail == nullptr || head == nullptr)
+			return;
+		else
 		{
+			linkedList<T> iList;
+			iList.head = head;
+			iList.tail = tail;
+			while (iList.head->next != iList.tail //EVEN CASE
+				&& iList.head != iList.tail) //ODD CASE
+			{
+				//when list is even
+				/*right.push_front(tail);
+				left.push_back(head);*/
+
+				iList.head = iList.head->next;
+				iList.tail = iList.tail->prev;
+			}
+
 			//when the list is odd
-			if (head == tail)
+			if (iList.head == iList.tail)
 			{
-				left.push_back(head);
+				left.head = head;
+				left.tail = iList.head;
+
+				right.head = iList.head->next;
+				right.tail = tail;
 			}
-			else
+			else //when even
 			{
-				right.push_front(tail);
-				left.push_back(head);
+				left.head = head;
+				left.tail = iList.head;
+
+				right.head = iList.tail;
+				right.tail = tail;
 			}
 
-			head = head->next;
-			tail = tail->prev;
+			//clear the list
+			head = nullptr;
+			tail = nullptr;
+
+			//set both ends of right and left to nullptr
+			right.head->prev = nullptr;
+			right.tail->next = nullptr;
+
+			left.head->prev = nullptr;
+			left.tail->next = nullptr;
 		}
-
-		//clear the list
-		head = nullptr;
-		tail = nullptr;
-
-		//set both ends of right and left to nullptr
-		right.head->prev = nullptr;
-		right.tail->next = nullptr;
-
-		left.head->prev = nullptr;
-		left.tail->next = nullptr;
-
 	}
 
 	//Uses simple selection sort
 	void sort()
 	{
-		for (node* iptr = head; iptr != nullptr; iptr = iptr->next)
-		{
-			//findsmallest
-			node* small = findSmallest(iptr);
+		linkedList<T> temp;
+		temp.head = head;
+		temp.tail = tail;
+		mergeSort(temp);
 
-			//swap smallest to node iptr
-			swap(iptr->data, small->data);
-		}
+		//now i need to swap temp with this
+		this->head = temp.head;
+		this->tail = temp.tail;
 	}
 
 	//Implement a method that takes 2 sorted lists and merges them
@@ -487,46 +282,326 @@ public:
 	//In this example, it should write all the words from Moby Dick to the provided output file in sorted order.
 	void writeToFile(string file);
 };
-//template <>
-//class linkedList <string>
-//{
-//private:
-//	class node
-//	{
-//		node* next;
-//		node* prev;
-//		string data;
-//	};
-//
-//	node* head;
-//	node* tail;
-//public:
-//	//sets head and tail to nullptr
-//	linkedList();
-//
-//	//adds the data to the back of the list
-//	void push_back(int _data);
-//
-//	//A simple print method
-//	void print();
-//
-//	//splits the contents of the current object in two halves. Each half goes to the left and right respectively. Runs in O(n)
-//	void split(linkedList& left, linkedList& right);
-//
-//	//Uses simple selection sort
-//	void sort();
-//
-//	//Implement a method that takes 2 sorted lists and merges them
-//	//into your (initially empty) list to create one sorted list.
-//	//Must run in O(n) time, where n is the total number of items in the two given lists.
-//	void merge();
-//
-//	//loadFromFile should open the file of the specified name and
-//	//enter each item (words in the novel "Moby Dick" in this case)
-//	//in the linked list.
-//	void loadFromFile(string file);
-//
-//	//writeToFile should write each item in the linked list to the provided output file.
-//	//In this example, it should write all the words from Moby Dick to the provided output file in sorted order.
-//	void writeToFile(string file);
-//};
+
+template<>
+class linkedList <string>
+{
+private:
+	class node
+	{
+	public:
+		node* next;
+		node* prev;
+		string data;
+
+		node()
+			:next(nullptr), prev(nullptr), data(0)
+		{
+
+		}
+
+		node(string _data)
+			:data(_data), next(nullptr), prev(nullptr)
+		{
+
+		}
+
+		node(string _data, node* _next, node* _prev)
+			:data(_data), next(_next), prev(_prev)
+		{
+
+		}
+	};
+
+	node* head;
+	node* tail;
+
+	bool isListEmpty()
+	{
+		if (head == nullptr && tail == nullptr)
+			return true;
+		else
+			return false;
+	}
+
+	void mergeSort(linkedList& list)//doesn't need an array parameter because it has this
+	{
+		if (list.head != nullptr
+			&& list.tail != nullptr
+			&& list.tail != list.head)//when we have one item
+		{
+			//split the array into smaller pieces
+			linkedList<string> left;
+			linkedList<string> right;
+
+			list.split(left, right);
+
+			list.mergeSort(left);
+			//start piecing them back together
+			list.mergeSort(right);
+			//call merge function
+			list.merge(left, right);
+		}
+
+		return;
+	}
+
+	
+
+public:
+	//sets head and tail to nullptr
+	linkedList()
+		:head(nullptr), tail(nullptr)
+	{
+
+	}
+
+	void push_back(string _data)
+	{
+		//Create a new node
+		node* baby = new node(_data);
+
+		if (tail == nullptr) //list is empty)
+		{
+			tail = baby;
+			head = baby;
+			baby->next = nullptr;
+			baby->prev = nullptr;
+		}
+		else
+		{
+			//Point old last node's next ptr to baby node
+			tail->next = baby;
+
+			//Point baby's prev ptr to old last node
+			baby->prev = tail;
+
+			//Update tail ptr to be the new baby node
+			tail = baby;
+
+			//set next to nullpre
+			tail->next = nullptr;
+		}
+	}
+
+	void push_back(node* p)
+	{
+		if (tail == nullptr)
+		{
+			tail = p;
+			head = p;
+
+			tail->prev = nullptr;
+		}
+		else
+		{
+			tail->next = p;
+			p->prev = tail;
+			tail = p;
+
+		}
+	}
+
+	void push_front(node* p)
+	{
+		if (head == nullptr)
+		{
+			head = p;
+			tail = p;
+			head->next = nullptr;
+
+		}
+		else
+		{
+			head->prev = p;
+			p->next = head;
+			head = p;
+
+		}
+	}
+
+	//A simple print method
+	void print()
+	{
+		//check if empty
+		if (isListEmpty())
+		{
+			cout << "(empty)" << endl;
+			return;
+		}
+		else
+		{
+			for (node* iptr = head; iptr != nullptr; iptr = iptr->next)
+			{
+				cout << iptr->data << " ";
+			}
+			cout << endl << endl;
+		}
+	}
+
+	//splits the contents of the current object in two halves. Each half goes to the left and right respectively. Runs in O(n)
+	void split(linkedList& left, linkedList& right)
+	{
+		//check if nullptr
+		if (tail == nullptr || head == nullptr)
+			return;
+		else
+		{
+			linkedList<string> iList;
+			iList.head = head;
+			iList.tail = tail;
+			while (iList.head->next != iList.tail //EVEN CASE
+				&& iList.head != iList.tail) //ODD CASE
+			{
+				//when list is even
+				/*right.push_front(tail);
+				left.push_back(head);*/
+
+				iList.head = iList.head->next;
+				iList.tail = iList.tail->prev;
+			}
+
+			//when the list is odd
+			if (iList.head == iList.tail)
+			{
+				left.head = head;
+				left.tail = iList.head;
+
+				right.head = iList.head->next;
+				right.tail = tail;
+			}
+			else //when even
+			{
+				left.head = head;
+				left.tail = iList.head;
+
+				right.head = iList.tail;
+				right.tail = tail;
+			}
+
+			//clear the list
+			head = nullptr;
+			tail = nullptr;
+
+			//set both ends of right and left to nullptr
+			right.head->prev = nullptr;
+			right.tail->next = nullptr;
+
+			left.head->prev = nullptr;
+			left.tail->next = nullptr;
+		}
+	}
+
+	//Uses simple selection sort
+	void sort()
+	{
+		linkedList<string> temp;
+		temp.head = head;
+		temp.tail = tail;
+		mergeSort(temp);
+
+		//now i need to swap temp with this
+		this->head = temp.head;
+		this->tail = temp.tail;
+	}
+
+	//Implement a method that takes 2 sorted lists and merges them
+	//into your (initially empty) list to create one sorted list.
+	//Must run in O(n) time, where n is the total number of items in the two given lists.
+	void merge(linkedList& left, linkedList& right)
+	{
+		//we want to loop until the shortest ends, then attach the remaining list to this
+		while (left.head != nullptr && right.head != nullptr)
+		{
+			if (left.head->data.compare(right.head->data) > 0)
+			{
+				push_back(right.head->data);
+				right.head = right.head->next;
+			}
+			else if (left.head->data.compare(right.head->data) < 0)
+			{
+				push_back(left.head->data);
+				left.head = left.head->next;
+			}
+			else
+			{
+				push_back(left.head->data);
+				left.head = left.head->next;
+			}
+		}
+
+		//if the left is empty
+		if (left.head == nullptr && right.head != nullptr)
+		{
+			//get the remaining items from the right
+			tail->next = right.head;
+			right.head->prev = tail;
+		}
+		else if (right.head == nullptr && left.head != nullptr)//right is empty
+		{
+			//get the remaining items from the left
+			tail->next = left.head;
+			left.head->prev = tail;
+		}
+
+		//M.I.B. the left and right
+		left.head = nullptr;
+		left.tail = nullptr;
+
+		right.head = nullptr;
+		right.tail = nullptr;
+	}
+
+	//loadFromFile should open the file of the specified name and
+	//enter each item (words in the novel "Moby Dick" in this case)
+	//in the linked list.
+	void loadFromFile(string file)
+	{
+		fstream load;
+		load.open(file, fstream::in);
+		string in;
+		while(load >> in)
+		{
+			//make a new node
+			node* babynode = new node(in);
+			push_back(babynode);
+		
+			//ensure to resest in to being empty
+			in = "";		
+
+		}
+
+		//done with load
+		load.close();
+	}
+
+	//writeToFile should write each item in the linked list to the provided output file.
+	//In this example, it should write all the words from Moby Dick to the provided output file in sorted order.
+	void writeToFile(string file)
+	{
+		fstream write;
+		write.open(file, fstream::out);
+
+		//ensure that the thing is not empty
+		if (isListEmpty())
+		{
+			write << "List is empty" << endl;
+		}
+		else
+		{
+			long i = 0;
+			//start at head, copy data to file, then go to next node
+			for (node* iptr = head; iptr != nullptr; iptr = iptr->next)
+			{
+				if (++i % 50 == 0)
+					write << iptr->data << " " << endl;
+				else
+					write << iptr->data << " ";
+
+			}
+		}
+
+		//don't forget to close the file
+		write.close();
+	}
+};
